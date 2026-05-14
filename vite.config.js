ddcc4f00b687a18f1466e4b1d56a6e0306a07ba9
andy <cresand79@gmail.com>
2026-05-14 16:13:54 +0100

    build: use standard react plugin instead of swc

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-swc' // or '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ski-trip-hub/', // This must match your GitHub repo name exactly
+  base: '/ski-trip-hub/', 
 })
